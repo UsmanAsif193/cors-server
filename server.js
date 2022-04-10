fe66f0ed4673c4bcc52a6532b80d9865b8bb7a96
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-11 01:52:43 +0500

    links added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const corsProxy = require("cors-anywhere");
 // Listen on a specific host via the HOST environment variable.
 const host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable.
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 corsProxy
   .createServer({
@@ -15,6 +15,6 @@ corsProxy
     requireHeader: ["origin", "x-requested-with"],
     removeHeaders: ["cookie", "cookie2"],
   })
-  .listen(port, host, () => {
+  .listen(PORT, host, () => {
     console.log("Running CORS Anywhere on " + host + ":" + port);
   });
